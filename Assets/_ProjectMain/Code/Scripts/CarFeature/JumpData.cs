76d80a49a1d1c311b13e8f217bed30924f9c3233
Phuoc Loc Nguyen Tran <phuocloc742@gmail.com>
2025-09-11 14:35:50 +0700

    ChangeGitignore

--- Assets/_ProjectMain/Code/Scripts/CarFeature/JumpData.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public class JumpData : MonoBehaviour
-{
-    [Header("Jump Info")]
-    [SerializeField] public float jumpHeightScale = 1f;
-    [SerializeField] public float jumpPushScale = 1f;
-}
